d08b2ce02289af33982038fbb3fd9288690947ac
Ahmed Ibrahim <ahmedibrahimrip@gmail.com>
2023-07-01 18:28:30 +0300

    Initial Commit

---
+++ TrainingAppCorona/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TrainingAppCorona
+//
+//  Created by Ahmed Ibrahim on 01/07/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
